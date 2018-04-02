4a874fa851926befca614926363d3a4d423d9711
FerPerez13 <fer13vlc@gmail.com>
2018-04-03 00:32:12 +0200

    Primera toma de contacto y creación del entorno

---
+++ SoyTuQuiroApp/ClientForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace SoyTuQuiroApp
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.lastNameBox = new System.Windows.Forms.TextBox();
+            this.dniBox = new System.Windows.Forms.TextBox();
+            this.commentsBox = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.readButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellidos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(466, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DNI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Comentarios";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(12, 25);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(182, 20);
+            this.nameBox.TabIndex = 4;
+            // 
+            // lastNameBox
+            // 
+            this.lastNameBox.Location = new System.Drawing.Point(210, 25);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(241, 20);
+            this.lastNameBox.TabIndex = 5;
+            // 
+            // dniBox
+            // 
+            this.dniBox.Location = new System.Drawing.Point(469, 25);
+            this.dniBox.Name = "dniBox";
+            this.dniBox.Size = new System.Drawing.Size(100, 20);
+            this.dniBox.TabIndex = 6;
+            // 
+            // commentsBox
+            // 
+            this.commentsBox.Location = new System.Drawing.Point(12, 73);
+            this.commentsBox.Name = "commentsBox";
+            this.commentsBox.Size = new System.Drawing.Size(557, 96);
+            this.commentsBox.TabIndex = 7;
+            this.commentsBox.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(425, 243);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(443, 195);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(126, 54);
+            this.saveButton.TabIndex = 9;
+            this.saveButton.Text = "Guardar";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(443, 324);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(126, 54);
+            this.editButton.TabIndex = 10;
+            this.editButton.Text = "Editar";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(443, 384);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(126, 54);
+            this.deleteButton.TabIndex = 11;
+            this.deleteButton.Text = "Eliminar";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(443, 264);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(126, 54);
+            this.readButton.TabIndex = 12;
+            this.readButton.Text = "Actualizar";
+            this.readButton.UseVisualStyleBackColor = true;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 450);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.commentsBox);
+            this.Controls.Add(this.dniBox);
+            this.Controls.Add(this.lastNameBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "ClientForm";
+            this.Text = "Clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox lastNameBox;
+        private System.Windows.Forms.TextBox dniBox;
+        private System.Windows.Forms.RichTextBox commentsBox;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button readButton;
+    }
+}
---
+++ SoyTuQuiroApp/Main.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SoyTuQuiroApp
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clientsButton = new System.Windows.Forms.Button();
+            this.servicesButton = new System.Windows.Forms.Button();
+            this.visitsButton = new System.Windows.Forms.Button();
+            this.historyButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clientsButton
+            // 
+            this.clientsButton.Location = new System.Drawing.Point(12, 194);
+            this.clientsButton.Name = "clientsButton";
+            this.clientsButton.Size = new System.Drawing.Size(228, 113);
+            this.clientsButton.TabIndex = 0;
+            this.clientsButton.Text = "Clientes";
+            this.clientsButton.UseVisualStyleBackColor = true;
+            this.clientsButton.Click += new System.EventHandler(this.clientsButton_Click);
+            // 
+            // servicesButton
+            // 
+            this.servicesButton.Location = new System.Drawing.Point(12, 325);
+            this.servicesButton.Name = "servicesButton";
+            this.servicesButton.Size = new System.Drawing.Size(228, 113);
+            this.servicesButton.TabIndex = 1;
+            this.servicesButton.Text = "Servicios";
+            this.servicesButton.UseVisualStyleBackColor = true;
+            // 
+            // visitsButton
+            // 
+            this.visitsButton.Location = new System.Drawing.Point(258, 194);
+            this.visitsButton.Name = "visitsButton";
+            this.visitsButton.Size = new System.Drawing.Size(228, 113);
+            this.visitsButton.TabIndex = 2;
+            this.visitsButton.Text = "Visitas";
+            this.visitsButton.UseVisualStyleBackColor = true;
+            this.visitsButton.Click += new System.EventHandler(this.visitsButton_Click);
+            // 
+            // historyButton
+            // 
+            this.historyButton.Location = new System.Drawing.Point(258, 325);
+            this.historyButton.Name = "historyButton";
+            this.historyButton.Size = new System.Drawing.Size(228, 113);
+            this.historyButton.TabIndex = 3;
+            this.historyButton.Text = "Historial";
+            this.historyButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SoyTuQuiroApp.Properties.Resources.Nombre_Largo_Nueva_Q;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(471, 165);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.historyButton);
+            this.Controls.Add(this.visitsButton);
+            this.Controls.Add(this.servicesButton);
+            this.Controls.Add(this.clientsButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Main";
+            this.Text = "STQ Center";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clientsButton;
+        private System.Windows.Forms.Button servicesButton;
+        private System.Windows.Forms.Button visitsButton;
+        private System.Windows.Forms.Button historyButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
